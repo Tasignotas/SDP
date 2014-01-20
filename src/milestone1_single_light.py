e740bbb45c04a5fde1c72512d41f6ae5113db322
Ignotas Sulzenko S1137931 <s1137931@sms.ed.ac.uk>
2014-01-20 18:47:14 +0000

    Adding working code for the first milestone with a single light sensor

--- src/milestone1_single_light.py
+++ src/milestone1_single_light.py
@@ -1,4 +1,4 @@
-# By Ignotas
+#By Ignotas
 
 from nxt.sensor import *
 from nxt.motor import *
@@ -11,7 +11,7 @@ BRICK = connection.brick
 
 # Constants
 THRESHOLD_WHITE = 325
-SPEED_FACTOR = 5
+SPEED_FACTOR = 6
 SPEED = 5 * SPEED_FACTOR
 
 
@@ -24,10 +24,8 @@ class Robot:
     def __init__(self):
         self.MOTOR_L = Motor(BRICK, PORT_B)
         self.MOTOR_R = Motor(BRICK, PORT_C)
-        self.LIGHT_L = Light(BRICK, PORT_3)
-        self.LIGHT_R = Light(BRICK, PORT_2)
+        self.LIGHT_L = Light(BRICK, PORT_2)
         self.LIGHT_L.set_illuminated(True)
-        self.LIGHT_R.set_illuminated(True)
 
 
     def stop(self):
@@ -56,88 +54,14 @@ class Robot:
         self.MOTOR_R.run(right, True)
         
 
-
-
 class Attack(Robot):
 
-    def find_border(self):
-        intensity_left = self.LIGHT_L.get_sample()
-        intensity_right = self.LIGHT_R.get_sample()
 
-        border_found = False
-        while not border_found:
-            intensity_left = self.LIGHT_L.get_sample()
-            intensity_right = self.LIGHT_R.get_sample()
-
-            print 'LIGHT L:', intensity_left, 'LIGHT_R:', intensity_right
-
-            if intensity_left < THRESHOLD_WHITE and intensity_right < THRESHOLD_WHITE:
-                self.run(SPEED, SPEED)
-            else:
-                border_found = True
-                self.stop()
-                self.idle()
-                print 'Border Found!'
-                return True
-
-    def align_to_border(self, left=False, right=False):
-        if not (left or right):
-            return
-        self.reverse(10,10)
-        self.run()
-
-    def run_boundry(self):
-        self.find_border()
-        intensity_left = self.LIGHT_L.get_sample()
-        intensity_right = self.LIGHT_R.get_sample()        
+    def run_boundry(self):        
         while True:
             intensity_left = self.LIGHT_L.get_sample()
-            intensity_right = self.LIGHT_R.get_sample()
-            print 'LIGHT L:', intensity_left, 'LIGHT_R:', intensity_right
-            if intensity_left < THRESHOLD_WHITE and intensity_right < THRESHOLD_WHITE:
-                self.turn(-10, 10)
-            elif intensity_left > THRESHOLD_WHITE and intensity_right > THRESHOLD_WHITE:
-                self.turn(10, -10)
+            print 'LIGHT L:', intensity_left
+            if intensity_left < THRESHOLD_WHITE:
+                self.run(SPEED, SPEED + 1)    
             else:
-                self.run(SPEED, SPEED)
-
-
-
-# def stop():
-#     MOTOR_R.brake()
-#     MOTOR_L.brake()
-
-# def idle():
-#     MOTOR_R.idle()
-#     MOTOR_L.idle()
-
-# def turn_left(left=15, right=35):
-#     green_found = False
-#     while not green_found:
-#         intensity = LIGHT.get_sample()
-#         print intensity
-
-#         if intensity <   THRESHOLD_WHITE:
-#             # run(left, right)
-#             # stop()
-#             idle()
-#             green_found = True
-#             return True
-#         else:
-#             run(-left, right)
-
-
-# def run(left, right):
-#     MOTOR_L.run(left, True)
-#     MOTOR_R.run(right, True)
-
-# def reverse():
-#     MOTOR_R.run(-30, True)
-#     MOTOR_L.run(-30, True)
-
-# def forward(speed=35):
-#     MOTOR_L.run(speed, True)
-#     MOTOR_R.run(speed, True)
-
-# def follow_border():
-#     intensity = LIGHT.get_sample()
+                self.turn(5, -5)
