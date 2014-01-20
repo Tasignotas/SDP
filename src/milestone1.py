7d28e19a2a18fffba40bccf7b6b49112305b6c08
Ignotas Sulzenko S1137931 <s1137931@sms.ed.ac.uk>
2014-01-20 18:40:27 +0000

    Adding the code for the first milestone. All of the code runs from the brick

--- src/milestone1.py
+++ src/milestone1.py
@@ -8,8 +8,8 @@ connection = Connection(name='GRP7D')
 BRICK = connection.brick
 
 # Constants
-THRESHOLD_WHITE = 390
-SPEED_FACTOR = 5
+THRESHOLD_WHITE = 325
+SPEED_FACTOR = 6
 SPEED = 5 * SPEED_FACTOR
 
 
@@ -19,22 +19,24 @@ def is_border(intensity):
 
 class Robot:
 
-    MOTOR_L = Motor(BRICK, PORT_B)
-    MOTOR_R = Motor(BRICK, PORT_C)
-    LIGHT_L = Light(BRICK, PORT_3)
-    LIGHT_R = Light(BRICK, PORT_2)
+    def __init__(self):
+        self.MOTOR_L = Motor(BRICK, PORT_B)
+        self.MOTOR_R = Motor(BRICK, PORT_C)
+        self.LIGHT_L = Light(BRICK, PORT_2)
+        self.LIGHT_L.set_illuminated(True)
+
 
     def stop(self):
-        MOTOR_R.brake()
-        MOTOR_L.brake()
+        self.MOTOR_R.brake()
+        self.MOTOR_L.brake()
 
     def idle(self):
-        MOTOR_R.idle()
-        MOTOR_L.idle()
+        self.MOTOR_R.idle()
+        self.MOTOR_L.idle()
 
     def run(self, left=SPEED, right=SPEED):
-        MOTOR_L.run(left, True)
-        MOTOR_R.run(right, True)
+        self.MOTOR_L.run(left, True)
+        self.MOTOR_R.run(right, True)
 
     def reverse(self, left=SPEED, right=SPEED):
         self.run(-left, -right)
@@ -44,87 +46,20 @@ class Robot:
 
     def rotate_right(self, left=SPEED, right=SPEED):
         self.run(left, -right)
-
+        
+    def turn(self, left, right):
+        self.MOTOR_L.run(left, True)
+        self.MOTOR_R.run(right, True)
+        
 
 class Attack(Robot):
 
-    def __init__(self):
-        LIGHT_L.set_illuminated(True)
-        LIGHT_R.set_illuminated(True)
 
-    def find_border(self):
-        intenstiy_left = LIGHT_L.get_sample()
-        intensity_right = LIGHT_R.get_sample()
-
-        border_found = False
-        while not border_found:
-            intenstiy_left = LIGHT_L.get_sample()
-            intensity_right = LIGHT_R.get_sample()
-
-            print 'LIGHT L:', intenstiy_left, 'LIGHT_R:', intensity_right
-
-            if intensity_left < THRESHOLD_WHITE and intensity_right < THRESHOLD_WHITE:
-                run(SPEED, SPEED)
+    def run_boundry(self):        
+        while True:
+            intensity_left = self.LIGHT_L.get_sample()
+            print 'LIGHT L:', intensity_left
+            if intensity_left < THRESHOLD_WHITE:
+                self.run(SPEED, SPEED)    
             else:
-                border_found = True
-                # stop()
-                idle()
-                print 'Border Found!'
-                return True
-
-    def align_to_border(self, left=False, right=False):
-        if not (left or right):
-            return
-        self.reverse(10,10)
-        self.run()
-
-
-
-
-
-# def stop():
-#     MOTOR_R.brake()
-#     MOTOR_L.brake()
-
-# def idle():
-#     MOTOR_R.idle()
-#     MOTOR_L.idle()
-
-# def turn_left(left=15, right=35):
-#     green_found = False
-#     while not green_found:
-#         intensity = LIGHT.get_sample()
-#         print intensity
-
-#         if intensity <   THRESHOLD_WHITE:
-#             # run(left, right)
-#             # stop()
-#             idle()
-#             green_found = True
-#             return True
-#         else:
-#             run(-left, right)
-
-# def turn_right(left=35, right=15):
-#     MOTOR_L.run(left, True)
-#     MOTOR_R.run(right, True)
-
-# def run(left, right):
-#     MOTOR_L.run(left, True)
-#     MOTOR_R.run(right, True)
-
-# def reverse():
-#     MOTOR_R.run(-30, True)
-#     MOTOR_L.run(-30, True)
-
-# def forward(speed=35):
-#     MOTOR_L.run(speed, True)
-#     MOTOR_R.run(speed, True)
-
-# def follow_border():
-#     intensity = LIGHT.get_sample()
-
-
-
-
-
+                self.turn(5, -5)
