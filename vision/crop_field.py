52363f045ead266950478916b60cd5e783a1f9d8
Milan Pavlik <pavlik.mil@gmail.com>
2014-01-22 14:24:44 +0000

    Static measures used for cropping

--- vision/crop_field.py
+++ vision/crop_field.py
@@ -1,6 +1,6 @@
 from detect_field import *
 
-def get_crop_coordinates(frame, tolerance=0):
+def get_crop_coordinates(frame, width=520, height=285):
     """
     Get crop coordinated for the actual image based on masked version.
 
@@ -13,13 +13,19 @@ def get_crop_coordinates(frame, tolerance=0):
     fast = cv2.FastFeatureDetector()
 
     # get keypoints
-    kp = fast.detect(frame, None)
+    kp = fast.detect(mask, None)
 
     x_min = min(kp, key=lambda x: x.pt[0]).pt[0]
     y_min = min(kp, key=lambda x: x.pt[1]).pt[1]
     x_max = max(kp, key=lambda x: x.pt[0]).pt[0]
     y_max = max(kp, key=lambda x: x.pt[1]).pt[1]
 
+    x_delta = x_max - x_min
+    y_delta = y_max - y_min
+
+    x_remaining = (width - x_delta) / 2
+    y_remaining = (height - y_delta) / 2
+
     return (
-        x_min - tolerance, x_max + tolerance,
-        y_min - tolerance, y_max + tolerance)
+        x_min + x_remaining, x_max + x_remaining,
+        y_min + y_remaining, y_max + y_remaining)
