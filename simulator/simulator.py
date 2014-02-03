66b529eba91eebcca388171e7a61d86294951b47
victor-dumitrescu <vichvymetal@gmail.com>
2014-02-03 20:05:12 +0000

    Initialisation of simulator, robot images

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -2,41 +2,52 @@ import cv2
 
 
 class Simulator(object):
-	
-	def __init__(self):
-		self.clean = self._get_image('blabla')
+    
+    def __init__(self):
+        self.clean = cv2.imread('bg.jpg')
+        cv2.namedWindow('Simulator')
 
 
-	def run(self):
-		"""
-		Run the simulation.
-		"""
-		self._initialize()
 
-	def _initialize(self):
-		"""
-		Display an image and make the user select points for the
-		starting locations of the robots.
+    def run(self):
+        """
+        Run the simulation.
+        """
+        # self._initialize()
+        while(1):
+            cv2.imshow('Simulator', self.clean)
+            k = cv2.waitKey(5) & 0xFF
 
-		Should draw circles/squares in the locations selected to
-		provide visual feedback
+            if k == 27:
+                break
 
-		Params:
-			?? May decide to load this optionally from a file ??
+    def _initialize(self):
+        """
+        Display an image and make the user select points for the
+        starting locations of the robots.
 
-		returns None
-		"""
-		pass
+        Should draw circles/squares in the locations selected to
+        provide visual feedback
 
-	def _get_image(self, path):
-		"""
-		Get the basic pitch image for the simulator
+        Params:
+            ?? May decide to load this optionally from a file ??
 
-		Params:
-			[String] path		path to the image, relative to run_simulator.py
+        returns None
+        """
+        pass
 
-		Returns:
-			cv2 frame
-		"""
-		pass
+    def _get_image(self, path):
+        """
+        Get the basic pitch image for the simulator
 
+        Params:
+            [String] path       path to the image, relative to run_simulator.py
+
+        Returns:
+            cv2 frame
+        """
+        pass
+
+
+sim = Simulator()
+sim.run()
