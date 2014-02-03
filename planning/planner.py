8de7b84ecb75f7554bd37f9382ca49ea89a66686
Ignotas Sulzenko <tasignotas@gmail.com>
2014-02-03 19:45:06 +0000

    Modified planner to initialise correctly

--- planning/planner.py
+++ planning/planner.py
@@ -1,48 +1,40 @@
-from objects import *
+from models import World
 
 
 class Planner:
 
 
-    def __init__(self, home_pitch):
-        self.pitch = Pitch('../vision/calibrate.json')
-        zones = [0, 1, 2, 3]
-        goals = [Goal('goal_0', 0, 0, 20, 90), Goal('goal_3', 3, 0, 20, 270)]
-        self.ball = Ball('ball')
-        if not home_pitch:
-            zones.reverse()
-            goals.reverse()
-        self.h_defend = HomeRobot('home_defender', zones[0])
-        self.a_attack = AwayRobot('away_attacker', zones[1])
-        self.h_attack = HomeRobot('home_attacker', zones[2])
-        self.a_defend = AwayRobot('away_defender', zones[3])
-        self.h_goal = goals[0]
-        self.a_goal = goals[1]
+    def __init__(self, our_side):
+        self._world = World(our_side)
 
 
-    def plan(self, h_defend, h_attack, a_defend, a_attack, ball):
-        self.update([h_defend, h_attack, a_defend, a_attack, ball])
-
-        if self.h_defend.get_possession(self.ball):
+    def plan(self, position_dictionary):
+        self._world.update_positions(position_dictionary)
+        our_attacker = self._world.get_our_attacker()
+        our_defender = self._world.get_our_defender()
+        their_attacker = self._world.get_their_attacker()
+        their_defender = self._world.get_their_defender()
+        ball = self._world.get_ball() 
+        if our_defender.get_possession():
             # Match orientation with h_attack
             # Check for clear kick path
             # Kick ball or get MTV
             # Follow path to MTV point
             # If kick path clear then kick else repeat
             pass
-        elif self.h_attack.get_possession(self.ball):
+        elif our_attacker.get_possession():
             # Match orientation with a_goal
             # Check for clear kick path
             # Kick ball or get MTV
             # Follow path to MTV point
             # If kick path clear then kick else repeat
             pass
-        elif self.a_defend.get_possession(self.ball):
+        elif their_defender.get_possession():
             # Check for clear kick path
             # If clear calc MTV to block
             # Follow path to MTV point
             pass
-        elif self.a_attack.get_possession(self.ball):
+        elif their_attacker.get_possession():
             # Check for clear kick path
             # If clear calc MTV to block
             # Follow path to MTV point
@@ -50,14 +42,4 @@ class Planner:
         else:
             # Try to follow path to ball
             pass
-
         return ((0,0,0), (0,0,0))
-
-
-    def update(self, values):
-        i = 0
-        for item in [self.h_defend, self.h_attack, self.a_defend, self.a_attack, self.ball]:
-            item.set_position(values[i][0], values[i][1])
-            item.set_orientation(values[i][2])
-            item.set_velocity(values[i][3])
-            i += 1
