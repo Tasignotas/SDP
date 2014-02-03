f8df5dffeb34d64b25f7a127d8990575eecc2a92
Milan Pavlik <pavlik.mil@gmail.com>
2014-02-03 19:21:53 +0000

    minor update

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -1,7 +1,42 @@
+import cv2
 
 
 class Simulator(object):
-	pass
+	
+	def __init__(self):
+		self.clean = self._get_image('blabla')
+
 
 	def run(self):
+		"""
+		Run the simulation.
+		"""
+		self._initialize()
+
+	def _initialize(self):
+		"""
+		Display an image and make the user select points for the
+		starting locations of the robots.
+
+		Should draw circles/squares in the locations selected to
+		provide visual feedback
+
+		Params:
+			?? May decide to load this optionally from a file ??
+
+		returns None
+		"""
 		pass
+
+	def _get_image(self, path):
+		"""
+		Get the basic pitch image for the simulator
+
+		Params:
+			[String] path		path to the image, relative to run_simulator.py
+
+		Returns:
+			cv2 frame
+		"""
+		pass
+
