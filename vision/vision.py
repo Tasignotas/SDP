e9de079bf39a80a205d2f5a38ec22ca0172a940b
Milan Pavlik <pavlik.mil@gmail.com>
2014-02-05 16:54:54 +0000

    Refactoring Vision file

--- vision/vision.py
+++ vision/vision.py
@@ -10,6 +10,8 @@ from planning.models import Vector
 TEAM_COLORS = set(['yellow', 'blue'])
 SIDES = ['left', 'right']
 
+PROCESSING_DEBUG = False
+
 
 class Vision:
     """
@@ -63,6 +65,13 @@ class Vision:
     def _init_robot_trackers(self, side, color):
         """
         Initialize side assignment.
+
+        Params:
+            [string] side   - our side
+            [string] color  - our color
+
+        Returns:
+            None.
         """
         # Temporary: divide zones into section
         zone_size = int(math.floor(self.crop_values[1] / 4.0))
@@ -110,20 +119,12 @@ class Vision:
 
         # Draw positions
         self._draw(frame, positions)
-
-        # NO IDEA WHAT THIS DOES!
-        # if positions[4]:
-        #     objects[4].oldPos.append(positions[4][0])
-        # if len(objects[4].oldPos) > 5:
-        #     objects[4].oldPos.pop(0)
-        #for i in objects:
-        # print objects[4].oldPos
             
         # MULTIPROCESSING DEBUG
-
-        # if hasattr(os, 'getppid'):  # only available on Unix
-        #     print 'parent process:', os.getppid()
-        # print 'process id:', os.getpid()
+        if PROCESSING_DEBUG:
+            if hasattr(os, 'getppid'):  # only available on Unix
+                print 'Parent process:', os.getppid()
+            print 'Process id:', os.getpid()
 
         result = {
             'our_attacker': self.to_vector(positions[1]),
@@ -132,39 +133,55 @@ class Vision:
             'their_defender': self.to_vector(positions[2]),
             'ball': self.to_vector(positions[4])
         }
+
         return result
 
     def _draw(self, frame, positions):
-        # print positions
+        """
+        Draw positions from the trackers on the screen.
+
+        Params:
+            [np.array] frame    - the frame to draw on
+            [5-tuple] positions - positions of the robots and the ball
+
+        Returns:
+            None. Image is displayed
+        """
+        
+        # Hacky! Refactor!
         for position in positions[:4]:
             cv2.circle(frame, (int(position[0]), int(position[1])), 10, (255, 0, 0), 1)
 
         print positions[4]
 
         if positions[4] is not None and positions[4][0] is not None and positions[4][0][0] is not None and positions[4][0][1] is not None:
-            cv2.circle(frame, (int(positions[4][0][0]), int(positions[4][0][1])), 8, (0,0,255), 1)
-
-        # Draw results
-        # TODO: Convert to a process!
-        # for val in positions:
-        #     if val is not None:
-        #         cv2.circle(frame, (val[0][0], val[0][1]), 10, (0, 255, 0), 1)
-        # for i in range(4):
-        #     if positions[i] is not None and positions[i][2] is not None:
-        #         cv2.circle(frame, (positions[i][2][0],positions[i][2][1]),5,(255,0,0),1)
-        #         cv2.line(frame,(positions[i][0][0],positions[i][0][1]),(positions[i][2][0],positions[i][2][1]),(0,0,255),3)
+            cv2.circle(frame, (int(positions[4][0][0]), int(positions[4][0][1])), 7, (0,0,255), 1)
 
         cv2.imshow('SUCH VISION', frame)
         cv2.waitKey(4)
 
     def _trim_image(self, frame):
-        # Trim the image
+        """
+        Given a frame, crop it to the size given in self.crop_values
+
+        Returns:
+            Cropped image
+        """
         return frame[
             self.crop_values[2]:self.crop_values[3],
             self.crop_values[0]:self.crop_values[1]
         ]
 
     def _run_trackers(self, frame):
+        """
+        Run trackers as separate processes
+
+        Params:
+            [np.frame] frame        - frame to run trackers on
+
+        Returns:
+            [5-tuple] positions     - locations of the robots and the ball
+        """
         queues = [Queue() for i in range(5)]
         objects = [self.us[0], self.us[1], self.opponents[0], self.opponents[1], self.ball_tracker]
 
@@ -195,4 +212,3 @@ class Vision:
         if args is not None:
             x, y = args[0] if args[0] is not None else (None, None)
             return Vector(x, y, args[1], args[2])
-
