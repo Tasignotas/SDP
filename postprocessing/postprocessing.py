c3e89160cc8f389e1058697aec6fe4bde1e423b6
Ignotas <tasignotas@gmail.com>
2014-02-05 21:08:43 +0000

    Adding the weighted average of the robot's angle

--- postprocessing/postprocessing.py
+++ postprocessing/postprocessing.py
@@ -3,6 +3,7 @@ from planning.models import Vector
 
 
 LAG = 0
+ANGLE_WEIGHTS = [0.258, 0.225, 0.196, 0.17, 0.148]
 
 
 class Postprocessing(object):
@@ -24,13 +25,28 @@ class Postprocessing(object):
 		"""
 		new_vector_dict = {}
 		for name, vec in vector_dict.iteritems():
-			new_vector_dict[name] = self.analyze_ball(vec) if name == 'ball' else self.analyze_robot(vec)
+			self._vectors[key] = [vec] + self._vectors[key]
+			self._vectors[key].pop()
+			new_vector_dict[name] = self.analyze_ball(key, vec) if name == 'ball' else self.analyze_robot(key, vec)
 		return new_vector_dict
 
 
-	def analyze_ball(self, current_vec):
-		pass
+	def analyze_ball(self, key, current_vec):
+		# This method calculates the angle and the velocity of the ball.
+		# TODO: make it able to PREDICT angle, speed and the location by specified lag.
 
 
-	def analyze_robot(self, current_vec):
-		pass
+		return current_vec
+
+
+	def analyze_robot(self, key, current_vec):
+		# This method calculates the angle and the velocity of the robot.
+		# TODO: make it able to PREDICT angle, speed and the location by specified lag.
+		previous_angles = [v.get_angle() if v else None for v in self._vectors[key]]		
+		angle_mult = sum([a if m else 0 for (a, m) in zip(ANGLE_WEIGHTS, previous_angles)])
+		if angle_mult:
+			angle = sum([(1.0 / angle_mult) * m.get_angle() if m else 0 for (a, m) in zip(ANGLE_WEIGHTS, previous_angles)])
+		else:
+			angle = None
+		
+		return Vector(current_vec.get_x(), current_vec.get_y(), angle, velocity)
