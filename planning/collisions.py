584f776a2a73fbf63ba6e7b6fd149979631486a2
alprint <rowan@alprint.co.uk>
2014-02-08 02:43:51 +0000

    Added method to get plan point close as possible to zone boundary

--- planning/collisions.py
+++ planning/collisions.py
@@ -2,9 +2,13 @@
 from math import hypot, atan2, cos, sin
 
 
-def get_overlap(path, robot):
-    # Get robot intersection with a given path
-    return path.overlaps(robot)
+def get_boundary_path(robot, (x, y, displacement, theta), zone):
+    # Get closest point on path to zone boundary
+    while not zone.isInside(x, y):
+        plan_x = robot.get_x() + ((displacement-1) * cos(theta))
+        plan_y = robot.get_y() + ((displacement-1) * sin(theta))
+        x, y, displacement = plan_x, plan_y, displacement-1
+    return x, y, displacement, theta
 
 
 def get_path_to_point(robot, x, y):
@@ -13,7 +17,7 @@ def get_path_to_point(robot, x, y):
     delta_y = y - robot.get_y()
     displacement = hypot(delta_x, delta_y)
     theta = atan2(delta_y, delta_x) - robot.get_angle()
-    return displacement, theta
+    return x, y, displacement, theta
 
 
 def get_interception(path, robot):
@@ -22,19 +26,22 @@ def get_interception(path, robot):
     return get_path_to_point(robot, center_path[0], center_path[1])
 
 
-def get_avoidance(path, robot):
+def get_avoidance(path, robot, obstacle):
     # Get path necessary for robot to avoid obstacles when kicking the ball
-    path_width_x = path[2][0] - path[3][0]
-    path_width_y = path[2][1] - path[3][1]
-    path_width = hypot(path_width_x, path_width_y)
-    robot_width = robot.get_dimensions()[0]
-    min_distance = (path_width * 0.5) + (robot_width * 0.5)
-    center_path = path.center()
-    delta_x = center_path[0] - robot.get_x()
-    delta_y = center_path[1] - robot.get_y()
-    displacement = hypot(delta_x, delta_y)
-    path_distance = min_distance - displacement
-    theta = atan2(delta_y, delta_x)
-    path_x = robot.get_x() + (path_distance * cos(theta))
-    path_y = robot.get_y() + (path_distance * sin(theta))
-    return get_path_to_point(robot, path_x, path_y)
+    if path.overlaps(obstacle):
+        path_width_x = path[2][0] - path[3][0]
+        path_width_y = path[2][1] - path[3][1]
+        path_width = hypot(path_width_x, path_width_y)
+        robot_width = obstacle.get_dimensions()[0]
+        min_distance = (path_width * 0.5) + (robot_width * 0.5)
+        center_path = path.center()
+        delta_x = center_path[0] - obstacle.get_x()
+        delta_y = center_path[1] - obstacle.get_y()
+        displacement = hypot(delta_x, delta_y)
+        path_distance = min_distance - displacement
+        theta = atan2(delta_y, delta_x)
+        path_x = robot.get_x() + (path_distance * cos(theta))
+        path_y = robot.get_y() + (path_distance * sin(theta))
+        return get_path_to_point(robot, path_x, path_y)
+    else:
+        return None
