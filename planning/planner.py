1450ba247d7d84f5a8d21f6af31ae1c1cf30b469
Ignotas <tasignotas@gmail.com>
2014-02-10 17:33:13 +0000

    Switching to the non-bounce code

--- planning/planner.py
+++ planning/planner.py
@@ -30,27 +30,12 @@ class Planner:
 
 
     def predict_y_intersection(self):
-        our_zone = self._world._pitch.get_zone(self._world.get_our_defender().get_zone())
-        our_x = min([x for (x, y) in our_zone[0]])
-        our_top_y = min([y for (x, y) in our_zone[0]])
-        our_bottom_y = max([y for (x, y) in our_zone[0]])
+        our_zone = self._world._pitch._zones[self._world.get_our_defender().get_zone()]
+        our_x = min([x for (x, y) in our_zone[0]]) if self._world.get_our_defender().get_zone() == 3 else max([x for (x, y) in our_zone[0]])
         their_angle = self._world.get_their_defender().get_angle()
         their_x = self._world.get_their_defender().get_x()
         their_y = self._world.get_their_defender().get_y()
-        print their_x, their_y, their_angle
-        print our_top_y
-        while their_x < our_x:
-            # If the ball is going to bounce off the wall:
-            if not (our_top_y < (their_y + tan(radians(their_angle)) * (our_x - their_x)) < our_bottom_y):
-                print 'Bounce!'
-                their_x += (our_top_y - their_y) / tan(radians(their_angle)) if tan(radians(their_angle)) < 0 else (our_bottom_y - their_y) / tan(radians(their_angle))
-                their_y = our_top_y if tan(radians(their_angle)) < 0 else our_bottom_y
-                their_angle = -their_angle
-                print 'x: ', their_x
-                print 'y: ', their_y
-                print 'angle: ', their_angle
-            else:
-                return (their_y + tan(radians(their_angle)) * (our_x - their_x))
+        return (their_y + tan(radians(their_angle)) * abs(our_x - their_x))
 
 
 
