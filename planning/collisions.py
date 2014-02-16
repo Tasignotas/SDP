82097cc3ea5c141b33245ed7e571202335e92a02
Ignotas Sulzenko <tasignotas@gmail.com>
2014-02-16 13:35:48 +0000

    Using properties in collisions.py

--- planning/collisions.py
+++ planning/collisions.py
@@ -5,18 +5,18 @@ from math import hypot, atan2, cos, sin
 def get_boundary_path(robot, (x, y, displacement, theta), zone):
     # Get closest point on path to zone boundary
     while not zone.isInside(x, y):
-        plan_x = robot.get_x() + ((displacement-1) * cos(theta))
-        plan_y = robot.get_y() + ((displacement-1) * sin(theta))
+        plan_x = robot.x + ((displacement-1) * cos(theta))
+        plan_y = robot.y + ((displacement-1) * sin(theta))
         x, y, displacement = plan_x, plan_y, displacement-1
     return x, y, displacement, theta
 
 
 def get_path_to_point(robot, x, y):
     # Get path for robot to a point (x, y)
-    delta_x = x - robot.get_x()
-    delta_y = y - robot.get_y()
+    delta_x = x - robot.x
+    delta_y = y - robot.y
     displacement = hypot(delta_x, delta_y)
-    theta = atan2(delta_y, delta_x) - robot.get_angle()
+    theta = atan2(delta_y, delta_x) - robot.angle
     return x, y, displacement, theta
 
 
@@ -35,13 +35,13 @@ def get_avoidance(path, robot, obstacle):
         robot_width = obstacle.get_dimensions()[0]
         min_distance = (path_width * 0.5) + (robot_width * 0.5)
         center_path = path.center()
-        delta_x = center_path[0] - obstacle.get_x()
-        delta_y = center_path[1] - obstacle.get_y()
+        delta_x = center_path[0] - obstacle.x
+        delta_y = center_path[1] - obstacle.y
         displacement = hypot(delta_x, delta_y)
         path_distance = min_distance - displacement
         theta = atan2(delta_y, delta_x)
-        path_x = robot.get_x() + (path_distance * cos(theta))
-        path_y = robot.get_y() + (path_distance * sin(theta))
+        path_x = robot.x + (path_distance * cos(theta))
+        path_y = robot.y + (path_distance * sin(theta))
         return get_path_to_point(robot, path_x, path_y)
     else:
         return None
