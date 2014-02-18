2d31df90c874aca9707f10f8fccd3fba00aa0804
Ignotas <tasignotas@gmail.com>
2014-02-18 19:53:57 +0000

    Fixed postprocessing and added unit tests

--- postprocessing/postprocessing.py
+++ postprocessing/postprocessing.py
@@ -7,20 +7,22 @@ class Postprocessing(object):
 
 	def __init__(self):
 		self._vectors = {}
-		self._vectors['ball']= Vector(0, 0, 0, 0)
-		self._vectors['our_attacker'] = Vector(0, 0, 0, 0)
-		self._vectors['their_attacker'] = Vector(0, 0, 0, 0)
-		self._vectors['our_defender'] = Vector(0, 0, 0, 0)
-		self._vectors['their_defender'] = Vector(0, 0, 0, 0)
+		self._vectors['ball']= {'vec' : Vector(0, 0, 0, 0), 'time' : 0}
+		self._vectors['our_attacker'] = {'vec' : Vector(0, 0, 0, 0), 'time' : 0}
+		self._vectors['their_attacker'] = {'vec' : Vector(0, 0, 0, 0), 'time' : 0}
+		self._vectors['our_defender'] = {'vec' : Vector(0, 0, 0, 0), 'time' : 0}
+		self._vectors['their_defender'] = {'vec' : Vector(0, 0, 0, 0), 'time' : 0}
+		self._time = 0
 
 	def analyze(self, vector_dict):
 		'''
 		This method analyzes current positions and previous object vector.
 		'''
+		self._time += 1
 		new_vector_dict = {}
 		for name, info in vector_dict.iteritems():
 			if name == 'ball':
-			new_vector_dict[name] = self.analyze_ball(info)
+				new_vector_dict[name] = self.analyze_ball(info)
 			else:
 				new_vector_dict[name] = self.analyze_robot(name, info)
 		return new_vector_dict
@@ -29,25 +31,33 @@ class Postprocessing(object):
 		'''
 		This method calculates the angle and the velocity of the ball.
 		'''
-		if info['x'] and info['y']:
-			delta_x = info['x'] - self._vectors['ball'].x
-			delta_y = info['y'] - self._vectors['ball'].y
-			velocity = hypot(delta_y, delta_x)
-			angle = atan2(delta_y, delta_x) % 2*pi
-			self_vectors['ball'] = Vector(info['x'], info['y'], angle, velocity)
+		if not(info['x'] == None) and not (info['y'] == None):
+			delta_x = info['x'] - self._vectors['ball']['vec'].x
+			delta_y = info['y'] - self._vectors['ball']['vec'].y
+			velocity = hypot(delta_y, delta_x)/(self._time - self._vectors['ball']['time'])
+			angle = atan2(delta_y, delta_x) % (2*pi)
+			self._vectors['ball']['vec'] = Vector(info['x'], info['y'], angle, velocity)
+			self._vectors['ball']['time'] =  self._time
 			return Vector(info['x'], info['y'], angle, velocity)
 		else:
-			return Vector(self._vectors['ball'].x, self._vectors['ball'].y, self._vectors['ball'].angle, self._vectors['ball'].velocity)
+			return Vector(self._vectors['ball']['vec'].x,
+						  self._vectors['ball']['vec'].y,
+						  self._vectors['ball']['vec'].angle,
+						  self._vectors['ball']['vec'].velocity)
 
-	def analyze_robot(self, key, current_vec):
+	def analyze_robot(self, key, info):
 		'''
 		This method calculates the angle and the velocity of the robot.
 		'''
-		if info['x'] and info['y'] and info['angle']:
-			delta_x = info['x'] - self._vectors[key].x
-			delta_y = info['y'] - self._vectors[key].y
-			velocity = hypot(delta_y, delta_x)
-			self_vectors[key] = Vector(info['x'], info['y'], info['angle'], velocity)
+		if not(info['x'] == None) and not(info['y'] == None) and not(info['angle'] == None):
+			delta_x = info['x'] - self._vectors[key]['vec'].x
+			delta_y = info['y'] - self._vectors[key]['vec'].y
+			velocity = hypot(delta_y, delta_x)/(self._time - self._vectors[key]['time'])
+			self._vectors[key]['vec'] = Vector(info['x'], info['y'], info['angle'], velocity)
+			self._vectors[key]['time'] = self._time
 			return Vector(info['x'], info['y'], info['angle'], velocity)
 		else:
-			return Vector(self._vectors[key].x, self._vectors[key].y, self._vectors[key].angle, self._vectors[key].velocity)		
+			return Vector(self._vectors[key]['vec'].x,
+						  self._vectors[key]['vec'].y,
+						  self._vectors[key]['vec'].angle,
+						  self._vectors[key]['vec'].velocity)		
