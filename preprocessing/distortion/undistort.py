9f13fb19ea56fa7c36a6a96463c5dfb19075d90a
victor-dumitrescu <vichvymetal@gmail.com>
2014-03-03 02:23:17 +0000

    Added a larger set of calibration images and a script to produce them. Also minor tweaks.

--- preprocessing/distortion/undistort.py
+++ preprocessing/distortion/undistort.py
@@ -5,23 +5,24 @@ import cv2
 import glob
 from copy import copy
 
-#chessboard dimensions (number of innner corners on each side)
+# Set the required dimension of all sample checkboard images
 dim = (9,6)
-
-# termination criteria
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 objp = np.zeros((dim[0]*dim[1], 3), np.float32)
 objp[:,:2] = np.mgrid[0:dim[0], 0:dim[1]].T.reshape(-1,2)
 
+# termination criteria
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
-images = glob.glob('samples/*.png')
+images = glob.glob('samples/pitch1/*.png')
 
 for fname in images:
+
     img = cv2.imread(fname)
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
@@ -30,6 +31,7 @@ for fname in images:
 
     # If found, add object points, image points (after refining them)
     if ret == True:
+
         objpoints.append(objp)
 
         corners2 = copy(corners)
@@ -37,6 +39,7 @@ for fname in images:
         imgpoints.append(corners2)
 
         # Draw and display the corners
+        # Comment this out to skip showing sample images!
         _ = cv2.drawChessboardCorners(img, dim, corners2, ret)
         cv2.imshow('img',img)
         cv2.waitKey(1000)
@@ -46,13 +49,22 @@ cv2.destroyAllWindows()
 # Get the calibration data
 ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
 
-img = cv2.imread('test.png')
-h,  w = img.shape[:2]
+capture = cv2.VideoCapture(0)
 
-#These are the actual values needed to undistort:
-newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),0,(w,h))
+c = True
+while c != 27:
+    status, frame = capture.read()
+    h,  w = frame.shape[:2]    
+    newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),0,(w,h))
 
-dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
-cv2.imwrite('calibresult.jpg',dst)
-cv2.imshow('img',dst)
-cv2.waitKey()
+    #These are the actual values needed to undistort:
+    dst = cv2.undistort(frame, mtx, dist, None, newcameramtx)
+
+    # crop the image
+    x,y,w,h = roi
+    dst = dst[y:y+h, x:x+w]
+
+    cv2.imshow('Original', frame)
+    cv2.imshow('Undistorted',dst)
+
+    c = cv2.waitKey(2) & 0xFF
