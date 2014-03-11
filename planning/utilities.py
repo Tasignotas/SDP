456d875433ad61936224080fac95207f7c854568
Ignotas <tasignotas@gmail.com>
2014-03-11 18:10:51 +0000

    Adding utilities@

---
+++ planning/utilities.py
@@ -0,0 +1,81 @@
+from math import tan, pi, hypot, log
+
+DISTANCE_MATCH_THRESHOLD = 15
+ANGLE_MATCH_THRESHOLD = pi/10
+BALL_ANGLE_THRESHOLD = pi/20
+MAX_DISPLACEMENT_SPEED = 690
+MAX_ANGLE_SPEED = 50
+
+def predict_y_intersection(world, predict_for_x, robot, full_width=False, bounce=False):
+        '''
+        Predicts the (x, y) coordinates of the ball shot by the robot
+        Corrects them if it's out of the bottom_y - top_y range.
+        If bounce is set to True, predicts for a bounced shot
+        Returns None if the robot is facing the wrong direction.
+        '''
+        x = robot.x
+        y = robot.y
+        top_y = world._pitch.height if full_width else world.our_goal.y + (world.our_goal.width/2)
+        bottom_y = 0 if full_width else world.our_goal.y - (world.our_goal.width/2)
+        angle = robot.angle
+        if (robot.x < predict_for_x and not (pi/2 < angle < 3*pi/2)) or (robot.x > predict_for_x and (3*pi/2 > angle > pi/2)):
+            if bounce:
+                if not (0 <= (y + tan(angle) * (predict_for_x - x)) <= world._pitch.height):
+                    bounce_pos = 'top' if (y + tan(angle) * (predict_for_x - x)) > world._pitch.height else 'bottom'
+                    x += (world._pitch.height - y) / tan(angle) if bounce_pos == 'top' else (0 - y) / tan(angle)
+                    y = world._pitch.height if bounce_pos == 'top' else 0
+                    angle = (-angle) % (2*pi)
+            predicted_y = (y + tan(angle) * (predict_for_x - x))
+            # Correcting the y coordinate to the closest y coordinate on the goal line:
+            if predicted_y > top_y:
+                return top_y
+            elif predicted_y < bottom_y:
+                return bottom_y
+            return predicted_y
+        else:
+            return None
+
+def grab_ball():
+    return {'left_motor' : 0, 'right_motor' : 0, 'kicker' : 0, 'catcher' : 1, 'speed' : 1000}
+
+def kick_ball():
+    return {'left_motor' : 0, 'right_motor' : 0, 'kicker' : 1, 'catcher' : 0, 'speed' : 1000}
+
+def open_catcher():
+    return {'left_motor' : 0, 'right_motor' : 0, 'kicker' : 1, 'catcher' : 0, 'speed' : 1000}
+
+def has_matched(robot, x=None, y=None, angle=None):
+    dist_matched = True
+    angle_matched = True
+    if not(x == None and y == None):
+        dist_matched = hypot(robot.x - x, robot.y - y) < DISTANCE_MATCH_THRESHOLD
+    if not(angle == None):
+        angle_matched = abs(angle) < ANGLE_MATCH_THRESHOLD
+    return dist_matched and angle_matched
+
+def calculate_motor_speed(displacement, angle, backwards_ok=False, careful=False):
+    '''
+    Simplistic view of calculating the speed: no modes or trying to be careful
+    '''
+    moving_backwards = False
+    general_speed = 95 if careful else 300
+    angle_thresh = BALL_ANGLE_THRESHOLD if careful else ANGLE_MATCH_THRESHOLD
+    if backwards_ok and abs(angle) > pi/2:
+        angle = (-pi + angle) if angle > 0 else (pi + angle)
+        moving_backwards = True
+    if not (displacement is None):
+        if displacement < DISTANCE_MATCH_THRESHOLD:
+            return {'left_motor' : 0, 'right_motor' : 0, 'kicker' : 0, 'catcher' : 0, 'speed' : general_speed}
+        elif abs(angle) > angle_thresh:
+            speed = (angle/pi) * MAX_ANGLE_SPEED
+            return {'left_motor' : -speed, 'right_motor' : speed, 'kicker' : 0, 'catcher' : 0, 'speed' : general_speed}
+        else:
+            speed = log(displacement, 10) * MAX_DISPLACEMENT_SPEED
+            speed = -speed if moving_backwards else speed
+            return {'left_motor' : speed, 'right_motor' : speed, 'kicker' : 0, 'catcher' : 0, 'speed' : general_speed}
+    else:
+        if abs(angle) > angle_thresh:
+            speed = (angle/pi) * MAX_ANGLE_SPEED
+            return {'left_motor' : -speed, 'right_motor' : speed, 'kicker' : 0, 'catcher' : 0, 'speed' : general_speed}
+        else:
+            return {'left_motor' : 0, 'right_motor' : 0, 'kicker' : 0, 'catcher' : 0, 'speed' : general_speed}
