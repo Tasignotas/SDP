50f9623e6ab64f79a527dc00a7fcf7a995c1ca61
Milan P <pavlik.mil@gmail.com>
2014-03-12 18:31:01 +0000

    Converting tabs to spaces.

--- planning/points_translator.py
+++ planning/points_translator.py
@@ -1,27 +1,28 @@
 class DistortionModule(object):
-	def __init__(self, width, height, x_correction, y_correction):
-		super(DistortionModule, self).__init__()
-		self.width = width  ## !!!! These will need to come from the size we've cropped the pitch to.
-		self.height = height
-		self.x_correction = x_correction  # TBC
-		self.y_correction = y_correction
+    def __init__(self, width, height, x_correction, y_correction):
+        super(DistortionModule, self).__init__()
+        self.width = width  ## !!!! These will need to come from the size we've cropped the pitch to.
+        self.height = height
+        self.x_correction = x_correction  # TBC
+        self.y_correction = y_correction
 
 tester = DistortionModule(640, 480, -0.016, -0.115)
 
+
 def correct_coordinates(xval, yval):
  #Normalization
-	norm_x = (2 * xval - tester.width) / float(tester.width)
-	norm_y = (2 * yval - tester.height) / float(tester.height)
+    norm_x = (2 * xval - tester.width) / float(tester.width)
+    norm_y = (2 * yval - tester.height) / float(tester.height)
 
-	radius = (norm_x * norm_x) + (norm_y * norm_y)
+    radius = (norm_x * norm_x) + (norm_y * norm_y)
 
-	moved_x = norm_x * (1 - tester.x_correction * radius)
-	moved_y = norm_y * (1 - tester.y_correction * radius)
-	
+    moved_x = norm_x * (1 - tester.x_correction * radius)
+    moved_y = norm_y * (1 - tester.y_correction * radius)
+
 #Rounding back
-	new_x = round((moved_x + 1) * tester.width / 2)
-	new_y = round((moved_y + 1) * tester.height / 2)
-	return(new_x, new_y)
+    new_x = round((moved_x + 1) * tester.width / 2)
+    new_y = round((moved_y + 1) * tester.height / 2)
+    return(new_x, new_y)
 
 
 ## I realise these are daft test cases
