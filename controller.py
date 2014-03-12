fc2e4b399e2412380bbca9661a29bb20bf6b7be3
Ryan Davies <ryanrenniedavies@gmail.com>
2014-03-12 16:17:36 +0000

    On/off switch for comms

--- controller.py
+++ controller.py
@@ -58,7 +58,7 @@ class Controller:
         self.planner = Planner(our_side=our_side)
 
         # Set up GUI
-        self.GUI = GUI(calibration=self.calibration)
+        self.GUI = GUI(calibration=self.calibration, arduino=self.arduino)
 
         self.color = color
         
@@ -238,31 +238,32 @@ class Arduino:
 
     def __init__(self,port,rate,timeOut,comms):
         self.serial = None
-        if comms >0:
-            self.comms = 1
-            try:
-                self.serial = serial.Serial(port,rate,timeout=timeOut)
-            except:
-                print "No Arduino detected!"
-                print "Continuing without comms."
-                self.comms = 0
-                #raise
-
-        else:
-            self.comms = 0
+        self.comms = comms
+        self.setComms(comms)
         self.port = port
         self.rate = rate
         self.timeout = timeOut
 
-    def flipComms(self):
-        if self.comms == 0 and self.serial == None:
-            try:
-                self.serial = serial.Serial(self.port,self.rate,self.timeout)
-            except:
-                print "No Arduino detected!"
-                print "Continuing without comms."
-                self.comms=1
-        self.comms = 1-self.comms
+    def setComms(self,comms):
+
+        if comms >0:
+            self.comms = 1
+            if self.serial == None:
+                try:
+                    self.serial = serial.Serial(port,rate,timeout=timeOut)
+                except:
+                    print "No Arduino detected!"
+                    print "Continuing without comms."
+                    self.comms = 0
+                    #raise
+        else:
+            #self.write('A_RUN_KICK\n')
+            self.write('A_RUN_ENGINE %d %d\n' % (0, 0))
+            #self.write('D_RUN_KICK\n')
+            self.write('D_RUN_ENGINE %d %d\n' % (0, 0))
+            self.comms = 0
+            
+
 
 
     def write(self,string):
