48b1160bf0c70eaed7dd49a55aa757679e1b1add
Milan P <pavlik.mil@gmail.com>
2014-03-13 13:52:55 +0000

    Implementing negative velocity

--- postprocessing/postprocessing.py
+++ postprocessing/postprocessing.py
@@ -48,9 +48,23 @@ class Postprocessing(object):
         This method calculates the angle and the velocity of the robot.
         '''
         if not(info['x'] is None) and not(info['y'] is None) and not(info['angle'] is None):
+
+            robot_angle = info['angle']
+
             delta_x = info['x'] - self._vectors[key]['vec'].x
             delta_y = info['y'] - self._vectors[key]['vec'].y
+
+            # Calculate the angle of the delta vector relative to (1, 0)
+            delta_angle = atan2(delta_y, delta_x)
+            # Offset the angle if negative, we only want positive values
+            delta_angle = delta_angle if delta_angle > 0 else 2 * pi + delta_angle
+
             velocity = hypot(delta_y, delta_x)/(self._time - self._vectors[key]['time'])
+
+            # Make the velocity negative if the angles are not roughly the same
+            if not (-pi / 2 < abs(delta_angle - robot_angle) < pi / 2):
+                velocity = -velocity
+
             self._vectors[key]['vec'] = Vector(info['x'], info['y'], info['angle'], velocity)
             self._vectors[key]['time'] = self._time
             return Vector(info['x'], info['y'], info['angle'], velocity)
